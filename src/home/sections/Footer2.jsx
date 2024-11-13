092dee258b015ec2d21c48d4fffee33922461981
chris <98842978+christopherkmoore1@users.noreply.github.com>
2024-11-13 10:13:09 -0800

    updates footer

--- src/home/sections/Footer2.jsx
+++ src/home/sections/Footer2.jsx
@@ -6,7 +6,7 @@ import {
 import { makeStyles } from '@mui/styles';
 import clsx from "clsx";
 import React from "react";
-import InstagramIcon from "../common/icons/InstagramIcon";
+import GithubIcon from "../common/icons/GithubIcon";
 import LinkedinIcon from "../common/icons/LinkedinIcon";
 import { NavLink } from "react-router-dom";
 
@@ -23,7 +23,9 @@ const useStyles = makeStyles(({ palette, ...theme }) => ({
     padding: 10,
     borderRadius: 4,
     marginRight: 12,
-    border: `1px solid ${palette.primary.contrastText}`,
+    "&:hover": {
+      background: darken("#011C3A", 0.2),
+    },
     cursor: "pointer",
   },
   link: {
@@ -58,24 +60,26 @@ const Footer2 = () => {
         <Grid container>
           <Grid item lg={6} md={6} sm={12}>
             <div className="p-8 h-full elevation-z3">
-              <h4 className="text-20 mb-6 relative">About Me</h4>
+              <h4 className="text-20 mb-6 relative">
+              Christopher Moore
+              </h4>
               <p className="text-inherit">
-                Christopher Moore <br></br>
-                Expert Mobile Developer & Tech Enthusiast.
-                <br></br>
-                <br></br>
                 Check out some of my work, or reach me here:
               </p>
               <div className="flex flex-wrap">
                 <div className={classes.iconWrapper}>
                   <a href="https://github.com/christopherkmoore">
-                    <InstagramIcon className="text-13" fontSize="small" />
+                    <GithubIcon className="text-13" fontSize="small" />
                   </a>
                 </div>
                 <div className={classes.iconWrapper}>
                   <a href="https://www.linkedin.com/in/christopher-moore1/">
                     <LinkedinIcon className="text-13" fontSize="small" />
                   </a>
+                </div>
+                <div className={classes.iconWrapper}>
+                  <a href="https://www.paypal.com/donate/?business=P5YETTFZG73JG&no_recurring=0&currency_code=USD"><u>Consider buying me a coffee ☕️ </u></a>
+
                 </div>
               </div>
             </div>
